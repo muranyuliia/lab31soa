87ee2edd8bdff2ae995fcfac4ccc904f18d74c7e
muranyuliia <146208756+muranyuliia@users.noreply.github.com>
2023-10-05 23:33:21 +0300

    ggghg

---
+++ Project1/Project1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double x;
+    double y;
+    double A;
+    double B;
+
+    cout << " x = "; cin >> x;
+    A = 2 * x - 13.5;
+
+    // спосіб 1: розгалуження в скороченій формі
+    if (x < -1)
+        B = sin(x) / (1 + pow(cos(x), 2) * x);
+    if (-1 <= x && x <= 1)
+        B = pow(cos(x), 2) * pow(sin(x), 2) * x - 1;
+    if (x > 1)
+        B = log10(x + 0.4);
+
+    y = A + B;
+    cout << "1) y = " << y << endl;
+
+    // спосіб 2: розгалуження в повній формі
+    if (x < -1)
+        B = sin(x) / (1 + pow(cos(x), 2) * x);
+    else if (-1 <= x && x <= 1)
+        B = pow(cos(x), 2) * pow(sin(x), 2) * x - 1;
+    else
+        B = log10(x + 0.4);
+
+    y = A + B;
+    cout << "2) y = " << y << endl;
+
+    cin.get();
+    return 0;
+}
